9ecf650de9b71d782d07f25fba636a694f95b3a9
Havochalo <officialmail85@gmail.com>
2022-12-22 18:29:32 +1100

    forntend specialised

--- api/src/routers/userRouter.js
+++ api/src/routers/userRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { emitWarning } from "process";
-import { createUser, getUser } from "../models/UserModel.js";
+import { createUser, getUser } from "../models/userModel/UserModel.js";
 
 const router = express.Router();
 
@@ -20,7 +20,14 @@ router.post("/", async (req, res, next) => {
           message: "cannot create a new user",
         });
   } catch (error) {
+    // next(error);
+    // WE will catch the error and do the handling here
+    if (error.message.includes("E11000 duplicate key error collection")) {
+      (error.errorCode = 200),
+        (error.message = "Please enter new email address");
+    }
     next(error);
+    console.log(error);
   }
 });
 
@@ -33,8 +40,15 @@ router.post("/login", async (req, res, next) => {
 
     // The Optional Chaining Operator (?.) This
     result?._id
-      ? console.log("login Sucessfull")
-      : console.log("Login is not sucess");
+      ? res.json({
+          status: "success",
+          message: "Login is successful",
+          result,
+        })
+      : res.json({
+          status: "error",
+          message: "Login is not successful",
+        });
   } catch (error) {
     next(error);
   }
